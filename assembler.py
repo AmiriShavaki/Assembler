9e9d52822d9e67d1be9c8b8cdf4ac1de1fde2620
Mahdi Amiri Shavaki <mahdi_amiri98@comp.iust.ac.ir>
2021-04-29 23:30:35 +0430

    +sample input improved +R Type instructions fully developed

--- assembler.py
+++ assembler.py
@@ -1,3 +1,30 @@
+import numpy
+
+typeFormat = {"add": 'R', "addu": 'R', "addi": 'I',
+              "and": 'R', "andi": 'I', "nor": 'R',
+              "or": 'R', "ori": 'I', "sll": 'R',
+              "sra": 'R', "xor": 'R', "xori": 'I',
+              "slt": 'R', "sltu": 'R', "slti": 'I',
+              "beq": 'I', "bne": 'I', "j": 'J', "jal": 'J',
+              "lb": 'I', 'lbu': 'I', 'lw': 'I', 'lui': 'I',
+              "sb": 'I', 'sw': 'I'}
+funct = {"add": '100000', "addu": '100001',
+         'and': '100100', 'nor': '100111', 'or': '100101',
+         'sll': '000000', 'sra': '000011', 'xor': '100110',
+         'slt': '101010', 'sltu': '101011'}
+regNums = {'$zero': 0, '$0': 0, '$at': 1, '$v0': 2,
+           '$v1': 3, '$a0': 4, '$a1': 5, '$a2': 6,
+           '$a3': 7, '$t0': 8, '$t1': 9, '$t2': 10,
+           '$t3': 11, '$t4': 12, '$t5': 13, '$t6': 14,
+           '$t7': 15, '$t8': 24, '$t9': 25, '$s0': 16,
+           '$s1': 17, '$s2': 18, '$s3': 19, '$s4': 20,
+           '$s5': 21, '$s6': 22, '$s7': 23, '$k0': 26,
+           '$k1': 27, '$gp': 28, '$sp': 29, '$fp': 30,
+           '$ra': 31}
+
+def convertToBin(n, k):
+    return numpy.binary_repr(n, width = k)
+
 def deleteComments(line):
     for i in range(len(line)):
         if line[i] == '#':
@@ -24,6 +51,7 @@ inFile = open("Input.asm", 'r')
 outFile = open("output.txt", 'w')
 
 inputLines = inFile.readlines()
+machineCode = []
 
 labelLineInd = {}
 
@@ -32,6 +60,28 @@ for lineInd in range(len(inputLines)):
     inputLines[lineInd] = inputLines[lineInd].split()
     for i in range(len(inputLines[lineInd])):
         inputLines[lineInd][i] = deleteComma(inputLines[lineInd][i])
+    curMachineCode = ""
+    if not len(inputLines[lineInd]):
+        continue
+    instName = inputLines[lineInd][0]
+    if typeFormat[instName] == 'R':
+        curMachineCode += "0" * 6 #opcode
+        if instName == 'sll' or instName == 'sra':
+            curMachineCode += "0" * 5 #rs
+            curMachineCode += convertToBin(regNums[inputLines[lineInd][2]], 5) #rt
+            curMachineCode += convertToBin(regNums[inputLines[lineInd][1]], 5) #rd
+            curMachineCode += convertToBin(int(inputLines[lineInd][3]), 5) #shamt
+        else:
+            curMachineCode += convertToBin(regNums[inputLines[lineInd][2]], 5) #rs
+            curMachineCode += convertToBin(regNums[inputLines[lineInd][3]], 5) #rt
+            curMachineCode += convertToBin(regNums[inputLines[lineInd][1]], 5) #rd
+            curMachineCode += "0" * 5 #shamt
+        curMachineCode += funct[instName] #funct
+    elif typeFormat[instName] == 'I':
+        a = 2*2
+    elif typeFormat[instName] == 'J':
+        a = 2*2
+    print(inputLines[lineInd], curMachineCode)
 
 myPrint(inputLines)
 
--- input.asm
+++ input.asm
@@ -6,11 +6,21 @@ fact:
      beq $t0, $zero, ELSE
      addi $v0, $zero, 1 # if so, result is 1
      addi $sp, $sp, 8 # pop 2 items from stack
-     jr $ra # and return to after jal
+     j $ra # and return to after jal
 ELSE: addi $a0, $a0, -1 # n >= 1: argument gets (n - 1)
      jal fact # call fact with (n - 1)
      lw $a0, 0($sp) # restore original n
      lw $ra, 4($sp) # and return address
      addi $sp, $sp, 8 # pop 2 items from stack
-     mul $v0, $a0, $v0 # multipy to get result
-     jr $ra # and return to the caller
+     j $ra # and return to the caller
+sll $t2, $t0, 4
+or $t2, $t2, $t1
+sll $t2, $t0, 4
+andi $t2, $t2, -1
+sra $t2, $t0, 3
+andi $t2, $t2, 65519
+sw $t1, 32($t2)
+addi $t1, $0, $0
+LOOP: lw $s1, 0($s0)
+add $s2, $s2, $s1
+addi $s0, $s0, 4
